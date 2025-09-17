44469168c7323d167b97397758a7696db551a80a
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2025-09-17 19:11:02 +0330

    Update package configuration and Vite setup. Bump version to 0.1.0, add `vite-plugin-dts` for TypeScript definitions, and enhance build configuration for library output. Include additional dependencies and peer dependencies in `package.json` and update lock file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        dts({
+            insertTypesEntry: true,
+            rollupTypes: true,
+            tsconfigPath: "./tsconfig.app.json",
+        }),
+    ],
+    build: {
+        lib: {
+            entry: "src/index.ts",
+            name: "formBuilder",
+            fileName: (format) => `index.${format}.js`,
+            formats: ["es", "cjs"],
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
 });
